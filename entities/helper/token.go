da7f20251abf2886c5578879875deefcdb886dc4
shellrean <reanyusufali@gmail.com>
2020-12-21 11:07:51 +0700

    code: add extract, verity and retreive token

--- entities/helper/token.go
+++ entities/helper/token.go
@@ -1,6 +1,8 @@
 package helper
 
 import (
+	"strings"
+
 	"github.com/dgrijalva/jwt-go"
 
 	"github.com/shellrean/extraordinary-raport/domain"
@@ -33,4 +35,41 @@ func CreateRefreshToken(key string, user domain.User, td *domain.TokenDetails) (
 		return
 	}
 	return
+}
+
+func ExtractToken(bearer string) (res string) {
+	str := strings.Split(bearer, " ")
+	if len(str) == 2 {
+		res = str[1]
+		return
+	}
+	return
+}
+
+func VerifyToken(key string, tokenString string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, domain.ErrUnauthorized
+		}
+		return []byte(key), nil
+	})
+	if err != nil {
+		return nil, domain.ErrUnauthorized
+	}
+	return token, nil
+}
+
+func TokenValid(token *jwt.Token) error {
+	if _, ok := token.Claims.(jwt.Claims); !ok || !token.Valid {
+		return domain.ErrInvalidToken
+	}
+	return nil
+}
+
+func ExtractTokenMetadata(token *jwt.Token) map[string]interface{}{
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		return claims
+	}
+	return nil
 }
