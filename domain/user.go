83bc2d43cb61f4a9933fb0815ef72089664ee8c8
shellrean <reanyusufali@gmail.com>
2020-12-21 18:02:56 +0700

    code: add user cache repository interface

--- domain/user.go
+++ domain/user.go
@@ -27,10 +27,17 @@ type DTOUserLoginRequest struct {
 // UserUsecase represent the user's usecase
 type UserUsecase interface {
     Authentication(ctx context.Context, ur DTOUserLoginRequest) (t DTOTokenResponse, err error)
+    RefreshToken(ctx context.Context, ur DTOTokenResponse) (t DTOTokenResponse, err error)
 }
 
 // UserRepository represent the user's repository
 type UserRepository interface {
     Fetch(ctx context.Context, num int64) (res []User, err error)
     GetByEmail(ctx context.Context, email string) (res User, err error)
+}
+
+// UserCacheRepository represent the user's caching
+type UserCacheRepository interface {
+    StoreAuth(ctx context.Context, u User, td *TokenDetails) (err error)
+    DeleteAuth(ctx context.Context, uuid string) (err error)
 }
