847a51801348b83b7e1a2b8fd7dfce9b1470f341
shellrean <reanyusufali@gmail.com>
2020-12-21 11:06:28 +0700

    code: add unauthorized

--- domain/error.go
+++ domain/error.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrNotFound = errors.New("item not found")
 	ErrInvalidUser = errors.New("invalid user")
+	ErrUnauthorized = errors.New("unauthorized user")
+	ErrInvalidToken = errors.New("invalid token")
 )
