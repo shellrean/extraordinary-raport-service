2bd5f9ad36cb6c9fc737d53c54915ff4ad653760
shellrean <reanyusufali@gmail.com>
2020-12-21 08:16:41 +0700

    code: add func to create access & refresh token

--- entities/helper/token.go
+++ entities/helper/token.go
@@ -1,28 +1,36 @@
 package helper
 
 import (
-	"time"
-
 	"github.com/dgrijalva/jwt-go"
 
 	"github.com/shellrean/extraordinary-raport/domain"
 )
 
-func CreateToken(key string, user domain.User) (token string, err error) {
-	// Create access token
+func CreateAccessToken(key string, user domain.User, td *domain.TokenDetails) (err error) {
 	atClaims := jwt.MapClaims{}
 	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
 	atClaims["user_id"] = user.ID
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	atClaims["exp"] = td.AtExpires
 	
-	// Generate access token
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-	
-	// Get token signed
-	token, err = at.SignedString([]byte(key))
+	td.AccessToken, err = at.SignedString([]byte(key))
 	if err != nil {
-		return token, err
+		return
 	}
+	return
+}
 
+func CreateRefreshToken(key string, user domain.User, td *domain.TokenDetails) (err error) {
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = user.ID
+	rtClaims["exp"] = td.RtExpires
+
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(key))
+	if err != nil {
+		return
+	}
 	return
 }
