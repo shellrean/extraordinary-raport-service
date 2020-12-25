9e977c57c001f087aa99501eb00c01dc4292a434
shellrean <reanyusufali@gmail.com>
2020-12-25 08:37:37 +0700

    domain: add student

---
+++ domain/student.go
@@ -0,0 +1,48 @@
+package domain
+
+import (
+	"context"
+	"time"
+)
+
+type Parent struct {
+	Name 			string 		`json:"name"`
+	Address 		string 		`json:"address"`
+	Profession		string 		`json:"profession"`
+	Telp			string 		`json:"telp"`
+}
+
+type Familly struct {
+	Status			string 		`json:"status"`
+	Order			string 		`json:"order"`
+}
+
+type Student struct {
+	ID				int64		`json:"id"`
+	SRN				string 		`json:"srn"`
+	NSRN			string 		`json:"nsrn"`
+	Name			string 		`json:"name"`
+	Gender			string 		`json:"gender"`
+	BirthPlace		string 		`json:"birth_place"`
+	BirthDate		string		`json:"birth_date"`
+	Religion		Religion	`json:"religion"`
+	Address			string 		`json:"address"`
+	Telp			string 		`json:"telp"`
+	SchoolBefore	string 		`json:"school_before"`
+	AcceptedGrade	string 		`json:"accepted_grade"`
+	AcceptedDate	string 		`json:"accepted_date"`
+	Familly			Familly		`json:"familly"`
+	Father 			Parent		`json:"fater"`
+	Mother			Parent		`json:"mother"`
+	Guardian		Parent		`json:"guardian"`
+	CreatedAt		time.Time 	`json:"created_at"`
+	UpdatedAt		time.Time 	`json:"updated_at"`
+}
+
+type StudentRepository interface {
+	Fetch(ctx context.Context, num int64) (res []Student, err error)
+}
+
+type StudentUsecase interface {
+	Fetch(ctx context.Context, num int64) (res []Student, err error)
+}
