a027f2584840c57875f5b38241096de0b204794d
shellrean <reanyusufali@gmail.com>
2020-12-25 08:41:26 +0700

    code: change error response type

--- interface/http/middleware/main_middleware.go
+++ interface/http/middleware/main_middleware.go
@@ -3,8 +3,10 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 
+	"github.com/shellrean/extraordinary-raport/domain"
 	"github.com/shellrean/extraordinary-raport/config"
 	"github.com/shellrean/extraordinary-raport/entities/helper"
+	"github.com/shellrean/extraordinary-raport/interface/http/api"
 )
 
 type GoMiddleware struct {
@@ -27,14 +29,27 @@ func (m *GoMiddleware) CORS() gin.HandlerFunc {
 func (m *GoMiddleware) Auth() gin.HandlerFunc{
 	return func(c *gin.Context) {
 		tokenString := helper.ExtractToken(c.GetHeader("Authorization"))
+		if tokenString == "" {
+			c.AbortWithStatusJSON(
+				api.GetHttpStatusCode(domain.ErrHeaderMiss),
+				api.ResponseError(domain.ErrHeaderMiss.Error(), helper.GetErrorCode(domain.ErrHeaderMiss)),
+			)
+			return
+		}
 		token, err := helper.VerifyToken(m.cfg.JWTAccessKey, tokenString)
 		if err != nil {
-			c.AbortWithStatusJSON(helper.GetStatusCode(err), gin.H{"message": err.Error()})
+			c.AbortWithStatusJSON(
+				api.GetHttpStatusCode(err),
+				api.ResponseError(err.Error(), helper.GetErrorCode(err)),
+			)
 			return
 		}
 		err = helper.TokenValid(token)
 		if err != nil {
-			c.AbortWithStatusJSON(helper.GetStatusCode(err), gin.H{"message": err.Error()})
+			c.AbortWithStatusJSON(
+				api.GetHttpStatusCode(err), 
+				api.ResponseError(err.Error(), helper.GetErrorCode(err)),
+			)
 			return
 		}
 		data := helper.ExtractTokenMetadata(token)
