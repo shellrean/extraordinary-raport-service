7bcea3dab2c82362c3206b627b26e5b3eb4361ef
shellrean <reanyusufali@gmail.com>
2020-12-25 19:10:25 +0700

    code: crud opeartion repository

--- services/user/repository/postgres/main_user.go
+++ services/user/repository/postgres/main_user.go
@@ -3,6 +3,7 @@ package postgres
 import (
     "context"
     "database/sql"
+    "fmt"
 
     "github.com/shellrean/extraordinary-raport/domain"
 )
@@ -49,15 +50,30 @@ func (m *postgresUserRepository) fetch(ctx context.Context, query string, args .
     return
 }
 
-func (m *postgresUserRepository) Fetch(ctx context.Context, num int64) (res []domain.User, err error) {
+func (m *postgresUserRepository) Fetch(ctx context.Context, cursor int64, num int64) (res []domain.User, err error) {
     query := `SELECT id,name,email,password,created_at,updated_at
-            FROM users ORDER BY created_at LIMIT $1`
+            FROM users WHERE id > $1 ORDER BY id LIMIT $2`
 
-    res, err = m.fetch(ctx, query, num)
+    res, err = m.fetch(ctx, query, cursor, num)
     if err != nil {
         return nil, err
     }
 
+    return
+}
+
+func (m *postgresUserRepository) GetByID(ctx context.Context, id int64) (res domain.User, err error) {
+    query := `SELECT id,name,email,password,created_at,updated_at
+            FROM users WHERE id=$1`
+
+    list, err := m.fetch(ctx, query, id)
+    if err != nil {
+        return domain.User{}, err
+    }
+    if len(list) < 1 {
+        return domain.User{}, err
+    }
+    res = list[0]
     return
 }
 
@@ -76,5 +92,63 @@ func (m *postgresUserRepository) GetByEmail(ctx context.Context, email string) (
 
     res = list[0]
 
+    return
+}
+
+func (m *postgresUserRepository) Store(ctx context.Context, u *domain.User) (err error) {
+    query := `INSERT INTO users (name, email, password, created_at, updated_at)
+            VALUES ($1,$2,$3,$4,$5)`
+        
+    result, err := m.Conn.ExecContext(ctx, query, u.Name, u.Email, u.Password, u.CreatedAt, u.UpdatedAt)
+    if err != nil {
+        return err
+    }
+    rows, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    if rows != 1 {
+        return fmt.Errorf("expected single row affected, got %d rows affected", rows)
+    }
+
+    userId, err := result.LastInsertId()
+    if err != nil {
+        return err
+    }
+    u.ID = userId
+    return
+}
+
+func (m *postgresUserRepository) Update(ctx context.Context, u *domain.User) (err error) {
+    query := `UPDATE users SET name=$1, email=$2, password=$3, updated_at=$4
+            WHERE id=$5`
+
+    result, err := m.Conn.ExecContext(ctx, query, u.Name, u.Email, u.Password, u.UpdatedAt, u.ID)
+    if err != nil {
+        return err
+    }
+    rows, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    if rows != 1 {
+        return fmt.Errorf("expected single row affected, got %d rows affected", rows)
+    }
+    return
+}
+
+func (m *postgresUserRepository) Delete(ctx context.Context, id int64) (err error) {
+    query := `DELETE FROM users WHERE id=$1`
+    result, err := m.Conn.ExecContext(ctx, query, id)
+    if err != nil {
+        return err
+    }
+    rows, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    if rows != 1 {
+        return fmt.Errorf("expected single row affected, got %d rows affected", rows)
+    }
     return
 }
