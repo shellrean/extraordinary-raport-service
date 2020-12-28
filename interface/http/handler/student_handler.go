58f4fac9bdfaa9b6b87559b0a46ae3291b9081a4
shellrean <reanyusufali@gmail.com>
2020-12-28 17:27:50 +0700

    :green_heart: feature: get cursor and limit adn set header for next cursor

--- interface/http/handler/student_handler.go
+++ interface/http/handler/student_handler.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	// "github.com/go-playground/validator/v10"
@@ -10,24 +11,31 @@ import (
 	"github.com/shellrean/extraordinary-raport/domain"
 	"github.com/shellrean/extraordinary-raport/entities/helper"
 	"github.com/shellrean/extraordinary-raport/interface/http/api"
+	"github.com/shellrean/extraordinary-raport/interface/http/middleware"
 )
 
 type studentHandler struct {
 	studentUsecase		domain.StudentUsecase
 	config				*config.Config
+	mddl 				*middleware.GoMiddleware
 }
 
-func NewStudentHandler(r *gin.Engine, m domain.StudentUsecase, cfg *config.Config) {
+func NewStudentHandler(r *gin.Engine, m domain.StudentUsecase, cfg *config.Config, mddl *middleware.GoMiddleware) {
 	handler := &studentHandler{
 		studentUsecase:	m,
 		config: cfg,
+		mddl:	mddl,
 	}
 
-	r.GET("/students", handler.Index)
+	r.GET("/students", handler.mddl.Auth(), handler.Index)
 }
 
 func (h *studentHandler) Index(c *gin.Context) {
-	res, err := h.studentUsecase.Fetch(c, int64(10))
+	limS, _ := c.GetQuery("limit")
+	lim, _ := strconv.Atoi(limS)
+	cursor, _ := c.GetQuery("cursor")
+
+	res, nextCursor, err := h.studentUsecase.Fetch(c, cursor, int64(lim))
 	if err != nil {
 		error_code := helper.GetErrorCode(err)
 		c.JSON(
@@ -36,5 +44,7 @@ func (h *studentHandler) Index(c *gin.Context) {
 		)
 		return
 	}
+
+	c.Header("X-Cursor", nextCursor)
 	c.JSON(http.StatusOK, api.ResponseSuccess("success",res))
 }
