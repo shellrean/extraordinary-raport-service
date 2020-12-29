5f705bf06afeb1c4616a9fd10b25d60ac1e06f9a
shellrean <reanyusufali@gmail.com>
2020-12-29 09:28:51 +0700

    :heavy_plus_sign: added: struct academic domain

---
+++ domain/academic.go
@@ -0,0 +1,27 @@
+package domain
+
+import (
+	"time"
+	"context"
+)
+
+type Academic struct {
+	ID 			int64 		`json:"id"`
+	Name 		string 		`json:"name"`
+	Semester 	uint8 		`json:"semester"`
+	CreatedAt 	time.Time 	`json:"created_at"`
+	UpdatedAt 	time.Time 	`json:"updated_at"`
+}
+
+type AcademicRepository interface {
+	Fetch(ctx context.Context) ([]Academic, error)
+	GetByID(ctx context.Context, id int64) (Academic, error)
+	GetByYearAndSemester(ctx context.Context, year int, semester int) (Academic, error)
+	Store(ctx context.Context, ac *Academic) (error)
+	Delete(ctx context.Context, id int64) (error)
+}
+
+type AcademicUsecase interface {
+	Fetch(ctx context.Context) ([]Academic, error)
+	Generate(ctx context.Context) (Academic, error)
+}
