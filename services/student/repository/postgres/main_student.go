692adf612e7b53a82c50451e36e31e141e18806c
shellrean <reanyusufali@gmail.com>
2020-12-29 07:38:58 +0700

    :heavy_plus_sign: added: get by id, update and store

--- services/student/repository/postgres/main_student.go
+++ services/student/repository/postgres/main_student.go
@@ -1,6 +1,7 @@
 package postgres
 
 import (
+	"fmt"
 	"context"
 	"database/sql"
 
@@ -87,4 +88,110 @@ func (m *postgresStudentRepository) Fetch(ctx context.Context, cursor int64, num
 	}
 
 	return
+}
+
+func (m *postgresStudentRepository) GetByID(ctx context.Context, id int64) (res domain.Student, err error) {
+	query := `SELECT id, srn, nsrn, name, gender, birth_place, birth_date, religion_id, address, telp,
+			school_before, accepted_grade, accepted_date, familly_status, familly_order, father_name,
+			father_address, father_profession, father_telp, mother_name, mother_address, mother_profession,
+			mother_telp, guardian_name, guardian_address, guardian_profession, guardian_telp, created_at, updated_at
+			FROM students WHERE id = $1`
+
+    list, err := m.fetch(ctx, query, id)
+    if err != nil {
+        return domain.Student{}, err
+    }
+    if len(list) < 1 {
+        return domain.Student{}, err
+    }
+    res = list[0]
+    return
+}
+
+func (m *postgresStudentRepository) Store(ctx context.Context, t *domain.Student) (err error) {
+	query := `INSERT INTO students (srn,nsrn,name,gender,birth_place,birth_date,religion_id,address,telp,
+		school_before,accepted_grade,accepted_date,familly_status,familly_order,father_name,father_address,father_profession,
+		father_telp,mother_name,mother_address,mother_profession,mother_telp,guardian_name,guardian_address,guardian_profession,
+		guardian_telp,created_at,updated_at) VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28)  RETURNING id`
+	err = m.Conn.QueryRowContext(ctx, query, 
+		t.SRN,
+		t.NSRN,
+		t.Name,
+		t.Gender,
+		t.BirthPlace,
+		t.BirthDate,
+		t.Religion.ID,
+		t.Address,
+		t.Telp,
+		t.SchoolBefore,
+		t.AcceptedGrade,
+		t.AcceptedDate,
+		t.Familly.Status,
+		t.Familly.Order,
+		t.Father.Name,
+		t.Father.Address,
+		t.Father.Profession,
+		t.Father.Telp,
+		t.Mother.Name,
+		t.Mother.Address,
+		t.Mother.Profession,
+		t.Mother.Telp,
+		t.Guardian.Name,
+		t.Guardian.Address,
+		t.Guardian.Profession,
+		t.Guardian.Telp,
+		t.CreatedAt,
+		t.UpdatedAt).Scan(&t.ID)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func (m *postgresStudentRepository) Update(ctx context.Context, t *domain.Student) (error) {
+	query := `UPDATE users SET (srn,nsrn,name,gender,birth_place,birth_date,religion_id,address,telp,
+		school_before,accepted_grade,accepted_date,familly_status,familly_order,father_name,father_address,father_profession,
+		father_telp,mother_name,mother_address,mother_profession,mother_telp,guardian_name,guardian_address,guardian_profession,
+		guardian_telp,updated_at) = ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27) WHERE id=$28`
+	result, err := m.Conn.ExecContext(ctx, query, 
+		t.SRN,
+		t.NSRN,
+		t.Name,
+		t.Gender,
+		t.BirthPlace,
+		t.BirthDate,
+		t.Religion.ID,
+		t.Address,
+		t.Telp,
+		t.SchoolBefore,
+		t.AcceptedGrade,
+		t.AcceptedDate,
+		t.Familly.Status,
+		t.Familly.Order,
+		t.Father.Name,
+		t.Father.Address,
+		t.Father.Profession,
+		t.Father.Telp,
+		t.Mother.Name,
+		t.Mother.Address,
+		t.Mother.Profession,
+		t.Mother.Telp,
+		t.Guardian.Name,
+		t.Guardian.Address,
+		t.Guardian.Profession,
+		t.Guardian.Telp,
+		t.UpdatedAt,
+		t.ID,
+	)
+	if err != nil {
+        return err
+    }
+    rows, err := result.RowsAffected()
+    if err != nil {
+        return err
+    }
+    if rows != 1 {
+        return fmt.Errorf("expected single row affected, got %d rows affected", rows)
+    }
+    return nil
 }
