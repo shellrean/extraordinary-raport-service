9968d6e4a7569b089c4ba09704eb76915d3cfb98
shellrean <reanyusufali@gmail.com>
2020-12-31 11:41:26 +0700

    :x: delete: dto token response from domain

--- domain/token.go
+++ domain/token.go
@@ -1,15 +1,10 @@
 package domain
 
-type TokenDetails struct {
+type Token struct {
 	AccessToken		string
 	RefreshToken	string
 	AccessUuid		string
 	RefreshUuid		string
 	AtExpires		int64
 	RtExpires		int64
-}
-
-type DTOTokenResponse struct {
-	AccessToken 	string 		`json:"access_token"`
-	RefreshToken	string 		`json:"refresh_token"`
 }
