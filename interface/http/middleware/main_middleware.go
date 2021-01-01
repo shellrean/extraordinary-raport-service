769317731e9136e51056e33fe4ab06923d0a63dc
shellrean <reanyusufali@gmail.com>
2021-01-01 09:40:27 +0700

    :green_heart: feat: exposed response header

--- interface/http/middleware/main_middleware.go
+++ interface/http/middleware/main_middleware.go
@@ -23,7 +23,8 @@ func (m *GoMiddleware) CORS() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Access-Control-Allow-Origin", "*")
         c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Expose-Headers", "X-Cursor, Content-Length, Content-Type")
         c.Header("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
         if c.Request.Method == "OPTIONS" {
