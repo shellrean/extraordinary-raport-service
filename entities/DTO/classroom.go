0aefc25e752bf8f9e0c00a8d95357b76e20a5a5e
shellrean <reanyusufali@gmail.com>
2021-01-02 20:30:50 +0700

    :heavy_plus_sign: add: classroom request dto struct

--- entities/DTO/classroom.go
+++ entities/DTO/classroom.go
@@ -6,3 +6,10 @@ type ClassroomResponse struct {
 	Grade 		string 		`json:"grade"`
 	MajorID 	int64 		`json:"major_id"`
 }
+
+type ClassroomRequest struct {
+	ID 			int64 		`json:"id"`
+	Name 		string 		`json:"name" validate:"required"`
+	Grade 		string 		`json:"grade" validate:"required"`
+	MajorID 	int64 		`json:"major_id" validate:"required"`
+}
