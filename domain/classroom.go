d4b96bc2790f42576150b36976af7448a47f6542
shellrean <reanyusufali@gmail.com>
2021-01-02 21:51:37 +0700

    :heavy_plus_sign: add: update & delete to interface

--- domain/classroom.go
+++ domain/classroom.go
@@ -18,10 +18,14 @@ type ClassroomRepository interface {
 	Fetch(ctx context.Context) ([]Classroom, error)
 	GetByID(ctx context.Context, id int64) (Classroom, error)
 	Store(ctx context.Context, c *Classroom) (error)
+	Update(ctx context.Context, c *Classroom) (error)
+	Delete(ctx context.Context, id int64) (error)
 }
 
 type ClassroomUsecase interface {
 	Fetch(ctx context.Context) ([]Classroom, error)
 	GetByID(ctx context.Context, id int64) (Classroom, error)
 	Store(ctx context.Context, c *Classroom) (error)
+	Update(ctx context.Context, c *Classroom) (error)
+	Delete(ctx context.Context, id int64) (error)
 }
