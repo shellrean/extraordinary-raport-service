24ec41783070888ecf881767861b75d826273585
shellrean <reanyusufali@gmail.com>
2021-01-04 09:57:54 +0700

    :heavy_plus_sign: add: classroom field and store interface (repo, usecase)

--- domain/classroom_academic.go
+++ domain/classroom_academic.go
@@ -8,6 +8,7 @@ import (
 type ClassroomAcademic struct {
 	ID 			int64		`json:"id"`
 	Academic 	Academic	`json:"academic"`
+	Classroom 	Classroom	`json:"classroom"`
 	Teacher 	User		`json:"teacher"`
 	CreatedAt	time.Time	`json:"created_at"`
 	UpdatedAt 	time.Time 	`json:"updated_at"`
@@ -15,8 +16,11 @@ type ClassroomAcademic struct {
 
 type ClassroomAcademicRepository interface {
 	Fetch(ctx context.Context) ([]ClassroomAcademic, error)
+	Store(ctx context.Context, ca *ClassroomAcademic) (error)
+	GetByAcademicAndClass(ctx context.Context, a int64, c int64) (ClassroomAcademic, error)
 }
 
 type ClassroomAcademicUsecase interface {
 	Fetch(ctx context.Context) ([]ClassroomAcademic, error)
+	Store(ctx context.Context, ca *ClassroomAcademic) (error)
 }
