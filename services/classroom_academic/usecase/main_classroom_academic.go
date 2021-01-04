036ccc770bb16b04ce4c0594ae2937e8a9519075
shellrean <reanyusufali@gmail.com>
2021-01-04 10:00:11 +0700

    :heavy_plus_sign: add: method store

--- services/classroom_academic/usecase/main_classroom_academic.go
+++ services/classroom_academic/usecase/main_classroom_academic.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"context"
 	"time"
+	"strconv"
 
 	"github.com/shellrean/extraordinary-raport/domain"
 	"github.com/shellrean/extraordinary-raport/config"
@@ -10,17 +11,26 @@ import (
 
 type classroomAcademicUsecase struct {
 	classAcademicRepo		domain.ClassroomAcademicRepository
+	settingRepo				domain.SettingRepository
+	userRepo				domain.UserRepository
+	classRepo				domain.ClassroomRepository
 	contextTimeout 			time.Duration
 	cfg 					*config.Config
 }
 
 func NewClassroomAcademicUsecase(
 	d 		domain.ClassroomAcademicRepository, 
+	s 		domain.SettingRepository,
+	u 		domain.UserRepository,
+	c 		domain.ClassroomRepository,
 	timeout time.Duration, 
 	cfg 	*config.Config,
 ) domain.ClassroomAcademicUsecase {
 	return &classroomAcademicUsecase {
 		classAcademicRepo:		d,
+		settingRepo:			s,
+		userRepo:				u,
+		classRepo:				c,
 		contextTimeout:			timeout,
 		cfg:					cfg,
 	}
@@ -39,5 +49,77 @@ func (u *classroomAcademicUsecase) Fetch(c context.Context) (res []domain.Classr
 		return
 	}
 
+	return
+}
+
+func (u *classroomAcademicUsecase) Store(c context.Context, ca *domain.ClassroomAcademic) (err error) {
+	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+	defer cancel()
+
+	res, err := u.settingRepo.GetByName(ctx, domain.SettingAcademicActive)
+	if err != nil {
+		if u.cfg.Release {
+			err = domain.ErrServerError
+			return
+		}
+		return
+	}
+
+	if res == (domain.Setting{}) {
+		err = domain.ErrNotFound
+		return
+	}
+
+	user, err := u.userRepo.GetByID(ctx, ca.Teacher.ID)
+	if err != nil {
+		if u.cfg.Release {
+			err = domain.ErrServerError
+			return
+		}
+		return
+	}
+
+	if user == (domain.User{}) {
+		err = domain.ErrNotFound
+		return
+	}
+
+	class, err := u.classRepo.GetByID(ctx, ca.Classroom.ID)
+	if err != nil {
+		if u.cfg.Release {
+			err = domain.ErrServerError
+			return
+		}
+		return
+	}
+
+	if class == (domain.Classroom{}) {
+		err = domain.ErrNotFound
+		return
+	}
+
+
+	id, err := strconv.Atoi(res.Value)
+	if err != nil {
+		if u.cfg.Release {
+			err = domain.ErrServerError
+			return
+		}
+		return
+	}
+
+	academic := domain.Academic{
+		ID: 	int64(id),
+	}
+
+	ca.Academic = academic
+	err = u.classAcademicRepo.Store(ctx, ca)
+	if err != nil {
+		if u.cfg.Release {
+			err = domain.ErrServerError
+			return
+		}
+		return
+	}
 	return
 }
