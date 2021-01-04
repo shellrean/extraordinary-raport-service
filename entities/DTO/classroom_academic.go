8b18af987d00ace8a9c5dc8f0f17d389f2d5e4dd
shellrean <reanyusufali@gmail.com>
2021-01-04 09:58:48 +0700

    :green_heart: implement: validation and classroom id

--- entities/DTO/classroom_academic.go
+++ entities/DTO/classroom_academic.go
@@ -2,6 +2,7 @@ package dto
 
 type ClassroomAcademicResponse struct {
 	ID 			int64		`json:"id"`
-	AcademicID 	int64		`json:"academic_id"`
-	TeacherID 	int64		`json:"teacher_id"`
+	AcademicID 	int64		`json:"academic_id" validate:"required"`
+	TeacherID 	int64		`json:"teacher_id" validate:"required"`
+	ClassroomID int64 		`json:"classroom_id" validate:"required"`
 }
