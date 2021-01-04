0d810fc637252cf7c970fd4fc3955201c1e60956
shellrean <reanyusufali@gmail.com>
2021-01-04 09:59:40 +0700

    :hammer: fix: issue with column classroom_id

--- services/classroom_academic/repository/postgres/main_classroom_academic.go
+++ services/classroom_academic/repository/postgres/main_classroom_academic.go
@@ -29,13 +29,16 @@ func (m *classroomAcademicRepository) fetch(ctx context.Context, query string, a
 
     result = []domain.ClassroomAcademic{}
     for rows.Next() {
-		t := domain.ClassroomAcademic{}
-		var academicID int64
-		var teacherId int64
+		t := domain.ClassroomAcademic{
+			Academic: domain.Academic{},
+			Classroom: domain.Classroom{},
+			Teacher: domain.User{},
+		}
         err = rows.Scan(
             &t.ID,
-			&academicID,
-			&teacherId,
+			&t.Academic.ID,
+			&t.Classroom.ID,
+			&t.Teacher.ID,
             &t.CreatedAt,
             &t.UpdatedAt,
         )
@@ -43,15 +46,6 @@ func (m *classroomAcademicRepository) fetch(ctx context.Context, query string, a
         if err != nil {
             return nil, err
 		}
-		
-		academic := domain.Academic {
-			ID:	academicID,
-		}
-		teacher := domain.User {
-			ID: teacherId,
-		}
-		t.Academic = academic
-		t.Teacher = teacher
 
         result = append(result, t)
     }
@@ -60,12 +54,43 @@ func (m *classroomAcademicRepository) fetch(ctx context.Context, query string, a
 }
 
 func (m *classroomAcademicRepository) Fetch(ctx context.Context) (res []domain.ClassroomAcademic, err error) {
-	query := `SELECT id, academic_id, teacher_id, created_at, updated_at
+	query := `SELECT id, academic_id, classroom_id, teacher_id, created_at, updated_at
 		FROM classroom_academics`
 	
 	res, err = m.fetch(ctx, query)
 	if err != nil {
 		return
 	}
+	return
+}
+
+func (m *classroomAcademicRepository) Store(ctx context.Context, ca *domain.ClassroomAcademic) (err error) {
+	query := `INSERT INTO classroom_academics (academic_id, classroom_id, teacher_id, created_at, updated_at)
+		VALUES($1,$2,$3,$4,$5) RETURNING id`
+	err = m.Conn.QueryRowContext(ctx, query, 
+		ca.Academic.ID,
+		ca.Classroom.ID,
+		ca.Teacher.ID,
+		ca.CreatedAt,
+		ca.UpdatedAt,
+	).Scan(&ca.ID)
+	if err != nil {
+		return
+	}
+	return 
+}
+
+func (m *classroomAcademicRepository) GetByAcademicAndClass(ctx context.Context, a int64, c int64) (res domain.ClassroomAcademic, err error) {
+	query := `SELECT id,academic_id,classroom_id, teacher_id,createdAt,updated_at
+		FROM classroom_academics WHERE academic_id=$1 AND classroom_id=$2`
+
+	list, err := m.fetch(ctx, query, a, c)
+	if err != nil {
+		return
+	}
+	if len(list) < 1 {
+		return domain.ClassroomAcademic{}, err
+	}
+	res = list[0]
 	return
 }
