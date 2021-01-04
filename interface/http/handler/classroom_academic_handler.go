319f2f535a4c2106d02998df8cd29665dab77e4e
shellrean <reanyusufali@gmail.com>
2021-01-04 09:59:12 +0700

    :heavy_plus_sign: add: store method

--- interface/http/handler/classroom_academic_handler.go
+++ interface/http/handler/classroom_academic_handler.go
@@ -2,8 +2,10 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/shellrean/extraordinary-raport/config"
     "github.com/shellrean/extraordinary-raport/domain"
@@ -35,6 +37,7 @@ func NewClassAcademicHandler(
 	ca.Use(handler.mddl.Auth())
 
 	ca.GET("/", handler.Fetch)
+	ca.POST("/", handler.Store)
 }	
 
 func (h *classAcademicHandler) Fetch(c *gin.Context) {
@@ -54,9 +57,68 @@ func (h *classAcademicHandler) Fetch(c *gin.Context) {
 			ID:				item.ID,
 			AcademicID:		item.Academic.ID,
 			TeacherID:		item.Teacher.ID,
+			ClassroomID: 	item.Classroom.ID,
 		}
 		data = append(data, ac)
 	}
 
 	c.JSON(http.StatusOK, api.ResponseSuccess("success", data)) 
+}
+
+func (h *classAcademicHandler) Store(c *gin.Context) {
+	var u dto.ClassroomAcademicResponse
+    if err := c.ShouldBindJSON(&u); err != nil {
+        err_code := helper.GetErrorCode(domain.ErrUnprocess)
+        c.JSON(
+            http.StatusUnprocessableEntity,
+            api.ResponseError(domain.ErrUnprocess.Error(), err_code),
+        )
+        return
+	}
+	validate := validator.New()
+    if err := validate.Struct(u); err != nil {
+        var reserr []api.ErrorValidation
+
+        errs := err.(validator.ValidationErrors)
+        for _, e := range errs {
+            msg := helper.GetErrorMessage(e)
+            res := api.ErrorValidation{
+                Field:      strings.ToLower(e.Field()),
+                Message:    msg,
+            }
+            reserr = append(reserr, res)
+        }
+        err_code := helper.GetErrorCode(domain.ErrValidation)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseErrorWithData(domain.ErrValidation.Error(), err_code, reserr),
+        )
+        return
+	}
+
+	ca := domain.ClassroomAcademic {
+		Academic: 	domain.Academic {
+			ID: 	u.AcademicID,
+		},
+		Teacher:	domain.User {
+			ID:		u.TeacherID,
+		},
+		Classroom: 	domain.Classroom {
+			ID: 	u.ClassroomID,
+		},
+	}
+
+	err := h.classAcademicUsecase.Store(c, &ca)
+	if err != nil {
+		err_code := helper.GetErrorCode(err)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseError(err.Error(), err_code),
+        )
+        return
+	}
+
+
+	u.ID = ca.ID
+	c.JSON(http.StatusOK, api.ResponseSuccess("success create academic classroom", u))
 }
