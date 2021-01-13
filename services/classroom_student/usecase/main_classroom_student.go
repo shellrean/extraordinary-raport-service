3374798b94edbf62b04eff8731cb0c7326773019
shellrean <reanyusufali@gmail.com>
2021-01-13 11:51:30 +0700

    :heavy_plus_sign: add: method fetch to classroom student usecase

---
+++ services/classroom_student/usecase/main_classroom_student.go
@@ -0,0 +1,59 @@
+package usecase 
+
+import (
+    "time"
+    "context"
+
+    "github.com/shellrean/extraordinary-raport/domain"
+    "github.com/shellrean/extraordinary-raport/config"
+    "github.com/shellrean/extraordinary-raport/entities/helper"
+)
+
+type csUsecase struct {
+	csRepo			domain.ClassroomStudentRepository
+	contextTimeout	time.Duration
+	cfg 			*config.Config
+}
+
+func NewClassroomStudentUsecase(
+	d domain.ClassroomStudentRepository,
+	timeout time.Duration,
+	cfg *config.Config,
+) domain.ClassroomStudentUsecase {
+	return &csUsecase {
+		csRepo:			d,
+		contextTimeout: timeout,
+		cfg:			cfg,
+	}
+}
+
+func (u *csUsecase) Fetch(c context.Context, cursor string, num int64) (res []domain.ClassroomStudent, nextCursor string, err error) {
+    if num == 0 {
+        num = int64(10)
+    }
+
+    ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+    defer cancel()
+
+    decodedCursor, err := helper.DecodeCursor(cursor)
+    if err != nil && cursor != "" {
+        err = domain.ErrBadParamInput
+        return
+    }
+
+    res, err = u.csRepo.Fetch(ctx, decodedCursor, num)
+    if err != nil {
+        if u.cfg.Release {
+            err = domain.ErrServerError
+            return
+        }
+        return
+    }
+
+    if len(res) == int(num) {
+        nextCursor = helper.EncodeCursor(res[len(res)-1].ID)
+    }
+
+    return
+}
+
