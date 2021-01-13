8fd45ef518e5a34f4e5ab5232a397f5614a9308e
shellrean <reanyusufali@gmail.com>
2021-01-13 20:06:15 +0700

    :heavy_plus_sign: add: update method classroom student interface

--- domain/classroom_student.go
+++ domain/classroom_student.go
@@ -17,10 +17,12 @@ type ClassroomStudentRepository interface {
 	Fetch(ctx context.Context, cursor int64, num int64) ([]ClassroomStudent, error)
 	GetByID(ctx context.Context, id int64) (ClassroomStudent, error)
 	Store(ctx context.Context, cs *ClassroomStudent) (error)
+	Update(ctx context.Context, cs *ClassroomStudent) (error)
 }
 
 type ClassroomStudentUsecase interface {
 	Fetch(ctx context.Context, cursor string, num int64) ([]ClassroomStudent, string, error)
 	GetByID(ctx context.Context, id int64) (ClassroomStudent, error)
 	Store(ctx context.Context, cs *ClassroomStudent) (error)
+	Update(ctx context.Context, cs *ClassroomStudent) (error)
 }
