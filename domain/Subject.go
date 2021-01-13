2d426f5ef0bb9d256f1f91c68ee426a18493d690
shellrean <reanyusufali@gmail.com>
2021-01-13 10:26:49 +0700

    :heavy_plus_sign: add: store to interface struct

--- domain/Subject.go
+++ domain/Subject.go
@@ -16,9 +16,11 @@ type Subject struct {
 type SubjectRepository interface {
 	Fetch(ctx context.Context, cursor int64, num int64) ([]Subject, error)
 	GetByID(ctx context.Context, id int64) (Subject, error)
+	Store(ctx context.Context, s *Subject) (error)
 }
 
 type SubjectUsecase interface {
 	Fetch(ctx context.Context, cursor string, num int64) ([]Subject, string, error)
 	GetByID(ctx context.Context, id int64) (Subject, error)
+	Store(ctx context.Context, s *Subject) (error)
 }
