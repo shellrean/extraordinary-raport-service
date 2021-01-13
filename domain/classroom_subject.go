e3b4c894b5293943afc4cda159a85f501f5793ae
shellrean <reanyusufali@gmail.com>
2021-01-14 06:59:08 +0700

    :heavy_plus_sign: add: classroom subject struct interface

---
+++ domain/classroom_subject.go
@@ -0,0 +1,24 @@
+package domain
+
+import (
+	"time"
+	"context"
+)
+
+type ClassroomSubject struct {
+	ID					int64
+	ClassroomAcademic	ClassroomAcademic
+	Subject 			Subject
+	Teacher				User
+	MGN					int
+	CreatedAt 			time.Time
+	UpdatedAt 			time.Time
+}
+
+type ClassroomSubjectRepository interface {
+	FetchByClassroom(ctx context.Context, class int64) ([]ClassroomSubject, error)
+}
+
+type ClassroomSubjectUsecase interface {
+	FetchByClassroom(ctx context.Context, class int64) ([]ClassroomSubject, error)
+}
