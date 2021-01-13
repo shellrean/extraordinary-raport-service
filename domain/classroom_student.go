ca29fa3b1a724dfa10b4cd297bb31c251eda651a
shellrean <reanyusufali@gmail.com>
2021-01-13 11:49:25 +0700

    :heavy_plus_sign: add: classroom student fetch method interface

--- domain/classroom_student.go
+++ domain/classroom_student.go
@@ -2,13 +2,21 @@ package domain
 
 import (
 	"time"
+	"context"
 )
 
 type ClassroomStudent struct {
 	ID 					int64
-	Academic 			Academic
 	ClassroomAcademic 	ClassroomAcademic
 	Student 			Student
 	CreatedAt 			time.Time
 	UpdatedAt 			time.Time
+}
+
+type ClassroomStudentRepository interface {
+	Fetch(ctx context.Context, cursor int64, num int64) ([]ClassroomStudent, error)
+}
+
+type ClassroomStudentUsecase interface {
+	Fetch(ctx context.Context, cursor string, num int64) ([]ClassroomStudent, string, error)
 }
