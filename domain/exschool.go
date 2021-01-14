42c135770ced715eb56daca4d0100451cc4c324c
shellrean <reanyusufali@gmail.com>
2021-01-14 07:00:05 +0700

    :heavy_plus_sign: add: exschool struct domain

---
+++ domain/exschool.go
@@ -0,0 +1,21 @@
+package domain
+
+import (
+	"time"
+	"context"
+)
+
+type Exschool struct {
+	ID 			int64
+	Name 		string
+	CreatedAt 	time.Time
+	UpdatedAt 	time.Time
+}
+
+type ExschoolRepository interface {
+	Fetch(ctx context.Context, cursor int64, num int64) ([]Exschool, error)
+}
+
+type ExschoolUsecase interface {
+	Fetch(ctx context.Context, cursor string, num int64) ([]Exschool, error)
+}
