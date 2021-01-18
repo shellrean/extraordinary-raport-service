8a7f0fba77ec33f2219fe2c147579719d0c5b323
shellrean <reanyusufali@gmail.com>
2021-01-18 08:48:17 +0700

    :heavy_plus_sign: add: delete multiple method for user_usecase and user_repository interface

--- domain/user.go
+++ domain/user.go
@@ -5,29 +5,27 @@ import (
     "context"    
 )
 
-// User ...
 type User struct {
-    ID          int64       `json:"id"`
-    Name        string      `json:"name" validate:"required"`
-    Email       string      `json:"email" validate:"required,email"`
-    Password    string      `json:"password" validate:"required,min=6"`
-    Role        int       `json:"role"`
-    CreatedAt   time.Time   `json:"created_at"`
-    UpdatedAt   time.Time   `json:"updated_at"`
+    ID          int64
+    Name        string
+    Email       string
+    Password    string
+    Role        int
+    CreatedAt   time.Time
+    UpdatedAt   time.Time
 }
 
-// UserUsecase represent the user's usecase
 type UserUsecase interface {
     Fetch(ctx context.Context, query string, cursor string, num int64) ([]User, string, error)
     GetByID(ctx context.Context, id int64) (User, error)
     Store(ctx context.Context, ur *User) (error)
     Update(ctx context.Context, ur *User) (error)
     Delete(ctx context.Context, id int64) (error)
+    DeleteMultiple(ctx context.Context, query string) (error)
     Authentication(ctx context.Context, ur User) (Token, error)
     RefreshToken(ctx context.Context, ur *Token) (error)
 }
 
-// UserRepository represent the user's repository
 type UserRepository interface {
     Fetch(ctx context.Context, query string, cursor int64, num int64) ([]User, error)
     GetByID(ctx context.Context, id int64) (User, error)
@@ -35,9 +33,9 @@ type UserRepository interface {
     Store(ctx context.Context, u *User) (error)
     Update(ctx context.Context, u *User) (error)
     Delete(ctx context.Context, id int64) (error)
+    DeleteMultiple(ctx context.Context, ids []string) (error)
 }
 
-// UserCacheRepository represent the user's caching
 type UserCacheRepository interface {
     StoreAuth(ctx context.Context, u User, td *Token) (error)
     DeleteAuth(ctx context.Context, uuid string) (error)
