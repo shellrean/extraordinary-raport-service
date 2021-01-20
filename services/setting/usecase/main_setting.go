ab510507139ca09ce9796ebe3d108e17dbcfa6ee
shellrean <reanyusufali@gmail.com>
2021-01-20 17:15:20 +0700

    :heavy_plus_sign: add: fetch method to setting usecase

---
+++ services/setting/usecase/main_setting.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/shellrean/extraordinary-raport/domain"
+    "github.com/shellrean/extraordinary-raport/config"
+)
+
+type settingUsecase struct {
+	settRepo		domain.SettingRepository
+	contextTimeout  time.Duration
+    cfg             *config.Config
+}
+
+func NewSettingUsecase(d domain.SettingRepository, timeout time.Duration, cfg *config.Config) domain.SettingUsecase {
+	return &settingUsecase {
+		settRepo:		d,
+		contextTimeout:	timeout,
+		cfg:			cfg,
+	}
+}
+
+func (u *settingUsecase) Fetch(c context.Context, names []string) (res []domain.Setting, err error) {
+	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+	defer cancel()
+	
+	res, err = u.settRepo.Fetch(ctx, names)
+    if err != nil {
+        if u.cfg.Release {
+            err = domain.ErrServerError
+            return
+        }
+        return
+	}
+	
+	return
+}
