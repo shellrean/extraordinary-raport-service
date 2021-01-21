0ca27e79a19acefd7d5c9fcb677b90cdee7bc65d
shellrean <reanyusufali@gmail.com>
2021-01-21 22:44:33 +0700

    :green_heart: implement: search in query Fetch method StudentRepository

--- services/student/repository/postgres/main_student.go
+++ services/student/repository/postgres/main_student.go
@@ -76,13 +76,13 @@ func (m *postgresStudentRepository) fetch(ctx context.Context, query string, arg
 	return result, nil
 }
 
-func (m *postgresStudentRepository) Fetch(ctx context.Context, cursor int64, num int64) (res []domain.Student, err error) {
+func (m *postgresStudentRepository) Fetch(ctx context.Context, q string, cursor int64, num int64) (res []domain.Student, err error) {
 	query := `SELECT id, srn, nsrn, name, gender, birth_place, birth_date, religion_id, address, telp,
 			school_before, accepted_grade, accepted_date, familly_status, familly_order, father_name,
 			father_address, father_profession, father_telp, mother_name, mother_address, mother_profession,
 			mother_telp, guardian_name, guardian_address, guardian_profession, guardian_telp, created_at, updated_at
-			FROM students WHERE id > $1 LIMIT $2`
-	res, err = m.fetch(ctx, query, cursor, num)
+			FROM students WHERE (LOWER(name) LIKE '%' || $1 || '%' OR srn LIKE '%' || $2 || '%') AND id > $3 LIMIT $4`
+	res, err = m.fetch(ctx, query, q, q, cursor, num)
 	if err != nil {
 		return nil, err
 	}
