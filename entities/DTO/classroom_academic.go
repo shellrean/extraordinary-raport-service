e1eca2fae9534e5f86c2b66a56b5fed4f1897713
shellrean <reanyusufali@gmail.com>
2021-01-21 08:50:55 +0700

    :heavy_plus_sign: add: ClassroomAcademicRequest struct to dto for classroom_academic domain

--- entities/DTO/classroom_academic.go
+++ entities/DTO/classroom_academic.go
@@ -1,8 +1,16 @@
 package dto
 
 type ClassroomAcademicResponse struct {
+	ID 				int64	`json:"id"`
+	AcademicID 		int64	`json:"academicID"`
+	TeacherID 		int64	`json:"teacherID"`
+	ClassroomID 	int64	`json:"classroomID"`
+	TeacherName 	string	`json:"teacherName"`
+	ClassroomName 	string 	`json:"classroomName"`
+}
+
+type ClassroomAcademicRequest struct {
 	ID 			int64		`json:"id"`
-	AcademicID 	int64		`json:"academic_id" validate:"required"`
-	TeacherID 	int64		`json:"teacher_id" validate:"required"`
-	ClassroomID int64 		`json:"classroom_id" validate:"required"`
+	TeacherID 	int64		`json:"teacherID" validate:"required"`
+	ClassroomID int64 		`json:"classroomID" validate:"required"`
 }
