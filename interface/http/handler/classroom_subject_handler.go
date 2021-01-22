e1334974f14c872607303667a401663da3278c2b
shellrean <reanyusufali@gmail.com>
2021-01-22 16:55:12 +0700

    :heavy_plus_sign: add: CRUD operations in http handler for ClassroomSubjectHandler

--- interface/http/handler/classroom_subject_handler.go
+++ interface/http/handler/classroom_subject_handler.go
@@ -2,9 +2,11 @@ package handler
 
 import (
 	"net/http"
+	"strings"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 
 	"github.com/shellrean/extraordinary-raport/config"
     "github.com/shellrean/extraordinary-raport/domain"
@@ -29,7 +31,188 @@ func NewClassroomSubjectHandler(r *gin.Engine, m domain.ClassroomSubjectUsecase,
 	csu := r.Group("/classroom-subjects")
 	csu.Use(handler.mddl.Auth())
 
+    csu.POST("/classroom-subject", handler.Store)
+    csu.GET("/classroom-subject/:id", handler.Show)
+    csu.PUT("/classroom-subject/:id", handler.Update)
+    csu.DELETE("/classroom-subject/:id", handler.Delete)
+
 	csu.GET("/classroom/:id", handler.FetchByClassroom)
+}
+
+func (h csuHandler) Store(c *gin.Context) {
+	var u dto.ClassroomSubjectRequest
+    if err := c.ShouldBindJSON(&u); err != nil {
+        err_code := helper.GetErrorCode(domain.ErrUnprocess)
+        c.JSON(
+            http.StatusUnprocessableEntity,
+            api.ResponseError(domain.ErrUnprocess.Error(), err_code),
+        )
+        return
+    }
+    validate := validator.New()
+    if err := validate.Struct(u); err != nil {
+        var reserr []api.ErrorValidation
+
+        errs := err.(validator.ValidationErrors)
+        for _, e := range errs {
+            msg := helper.GetErrorMessage(e)
+            res := api.ErrorValidation{
+                Field:      strings.ToLower(e.Field()),
+                Message:    msg,
+            }
+            reserr = append(reserr, res)
+        }
+        err_code := helper.GetErrorCode(domain.ErrValidation)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseErrorWithData(domain.ErrValidation.Error(), err_code, reserr),
+        )
+        return
+	}
+	
+	classroomSubject := domain.ClassroomSubject{
+		ClassroomAcademic:		domain.ClassroomAcademic{ID: u.ClassroomAcademicID},
+		Subject:				domain.Subject{ID: u.SubjectID},
+		Teacher:				domain.User{ID: u.TeacherID},
+		MGN:					u.MGN,
+	}
+
+	err := h.csuUsecase.Store(c, &classroomSubject)
+    if err != nil {
+        err_code := helper.GetErrorCode(err)
+        c.JSON(
+            api.GetHttpStatusCode(err),
+            api.ResponseError(err.Error(), err_code),
+        )
+        return
+	}
+	
+	u.ID = classroomSubject.ID
+	c.JSON(http.StatusOK, api.ResponseSuccess("create classroom subject success", u))
+}
+
+func (h csuHandler) Update(c *gin.Context) {
+    idS := c.Param("id")
+    id, err := strconv.Atoi(idS)
+    if err != nil {
+        err_code := helper.GetErrorCode(domain.ErrBadParamInput)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseError(domain.ErrBadParamInput.Error(), err_code),
+        )
+        return
+    }
+
+	var u dto.ClassroomSubjectRequest
+    if err := c.ShouldBindJSON(&u); err != nil {
+        err_code := helper.GetErrorCode(domain.ErrUnprocess)
+        c.JSON(
+            http.StatusUnprocessableEntity,
+            api.ResponseError(domain.ErrUnprocess.Error(), err_code),
+        )
+        return
+    }
+    validate := validator.New()
+    if err := validate.Struct(u); err != nil {
+        var reserr []api.ErrorValidation
+
+        errs := err.(validator.ValidationErrors)
+        for _, e := range errs {
+            msg := helper.GetErrorMessage(e)
+            res := api.ErrorValidation{
+                Field:      strings.ToLower(e.Field()),
+                Message:    msg,
+            }
+            reserr = append(reserr, res)
+        }
+        err_code := helper.GetErrorCode(domain.ErrValidation)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseErrorWithData(domain.ErrValidation.Error(), err_code, reserr),
+        )
+        return
+	}
+	
+	classroomSubject := domain.ClassroomSubject{
+        ID:                     int64(id),
+        ClassroomAcademic:		domain.ClassroomAcademic{ID: u.ClassroomAcademicID},
+		Subject:				domain.Subject{ID: u.SubjectID},
+		Teacher:				domain.User{ID: u.TeacherID},
+		MGN:					u.MGN,
+	}
+
+	err = h.csuUsecase.Update(c, &classroomSubject)
+    if err != nil {
+        err_code := helper.GetErrorCode(err)
+        c.JSON(
+            api.GetHttpStatusCode(err),
+            api.ResponseError(err.Error(), err_code),
+        )
+        return
+	}
+	
+	u.ID = classroomSubject.ID
+	c.JSON(http.StatusOK, api.ResponseSuccess("update classroom subject success", u))
+}
+
+func (h csuHandler) Show(c *gin.Context) {
+    idS := c.Param("id")
+    id, err := strconv.Atoi(idS)
+    if err != nil {
+        err_code := helper.GetErrorCode(domain.ErrBadParamInput)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseError(domain.ErrBadParamInput.Error(), err_code),
+        )
+        return
+    }
+    
+    res, err := h.csuUsecase.GetByID(c, int64(id))
+	if err != nil {
+		err_code := helper.GetErrorCode(err)
+        c.JSON(
+            api.GetHttpStatusCode(err),
+            api.ResponseError(err.Error(), err_code),
+        )
+        return
+    }
+    
+    data := dto.ClassroomSubjectResponse{
+        ID:						res.ID,
+		ClassroomAcademicID:	res.ClassroomAcademic.ID,
+		SubjectID:				res.Subject.ID,
+		SubjectName:			res.Subject.Name,
+		TeacherID:				res.Teacher.ID,
+		TeacherName:			res.Teacher.Name,
+		MGN:					res.MGN,
+    }
+
+    c.JSON(http.StatusOK, api.ResponseSuccess("success", data))
+}
+
+func (h csuHandler) Delete(c *gin.Context) {
+    idS := c.Param("id")
+    id, err := strconv.Atoi(idS)
+    if err != nil {
+        err_code := helper.GetErrorCode(domain.ErrBadParamInput)
+        c.JSON(
+            http.StatusBadRequest,
+            api.ResponseError(domain.ErrBadParamInput.Error(), err_code),
+        )
+        return
+    }
+
+    err = h.csuUsecase.Delete(c, int64(id))
+	if err != nil {
+		err_code := helper.GetErrorCode(err)
+        c.JSON(
+            api.GetHttpStatusCode(err),
+            api.ResponseError(err.Error(), err_code),
+        )
+        return
+    }
+
+    c.JSON(http.StatusOK, api.ResponseSuccess("success", make(map[string]string, 0)))
 }
 
 func (h csuHandler) FetchByClassroom(c *gin.Context) {
