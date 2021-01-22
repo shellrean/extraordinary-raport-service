8f6ae35b3e36c5b4e6b58bd573733c954719d444
shellrean <reanyusufali@gmail.com>
2021-01-22 16:52:01 +0700

    :heavy_plus_sign: add: method Store, GetByID, GetByClassroomAndSubject, Update,Delete to ClassroomSubjectRepository & ClassroomSubjectUsecase struct interface

--- domain/classroom_subject.go
+++ domain/classroom_subject.go
@@ -17,8 +17,17 @@ type ClassroomSubject struct {
 
 type ClassroomSubjectRepository interface {
 	FetchByClassroom(ctx context.Context, academicClassroomID int64) ([]ClassroomSubject, error)
+	Store(ctx context.Context, cs *ClassroomSubject) (error)
+	GetByID(ctx context.Context, id int64) (ClassroomSubject, error)
+	GetByClassroomAndSubject(ctx context.Context, academicClassroomID int64, subjectID int64) (ClassroomSubject, error)
+	Update(ctx context.Context, cs *ClassroomSubject) (error)
+	Delete(ctx context.Context, idi int64) (error)
 }
 
 type ClassroomSubjectUsecase interface {
 	FetchByClassroom(ctx context.Context, academicClassroomID int64) ([]ClassroomSubject, error)
+	Store(ctx context.Context, cs *ClassroomSubject) (error)
+	GetByID(ctx context.Context, id int64) (ClassroomSubject, error)
+	Update(ctx context.Context, cs *ClassroomSubject) (error)
+	Delete(ctx context.Context, idi int64) (error)
 }
