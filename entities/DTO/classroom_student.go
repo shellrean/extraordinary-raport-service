7aff0ebad98b3c8b6c1a7e74a771ac8836829db9
shellrean <reanyusufali@gmail.com>
2021-01-23 09:23:33 +0700

    :heavy_plus_sign: add: ClassroomStudentCopyRequest DTO

--- entities/DTO/classroom_student.go
+++ entities/DTO/classroom_student.go
@@ -14,3 +14,8 @@ type ClassroomStudentRequest struct {
 	ClassroomAcademicID int64		`json:"classroomAcademicID" validate:"required"`
 	StudentID 			int64		`json:"studentID" validate:"required"`
 }
+
+type ClassroomStudentCopyRequest struct {
+	ClassroomAcademicID 	int64 		`json:"classroomAcademicID"`
+	ToClassroomAcademicID 	int64 		`json:"toClassroomAcademicID"`
+}
