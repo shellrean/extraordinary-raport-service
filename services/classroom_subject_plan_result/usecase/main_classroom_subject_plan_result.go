76645eafb5be0894e32f8ec756de6586e69e0518
shellrean <reanyusufali@gmail.com>
2021-02-17 20:53:20 +0700

    :heavy_plus_sign: add: method export by plan to classroom-subject-plan result

--- services/classroom_subject_plan_result/usecase/main_classroom_subject_plan_result.go
+++ services/classroom_subject_plan_result/usecase/main_classroom_subject_plan_result.go
@@ -5,8 +5,11 @@ import (
 	"time"
 	"log"
 
+	"golang.org/x/sync/errgroup"
+
 	"github.com/shellrean/extraordinary-raport/domain"
 	"github.com/shellrean/extraordinary-raport/config"
+	"github.com/shellrean/extraordinary-raport/entities/helper"
 )
 
 type sprRepoT domain.ClassroomSubjectPlanResultRepository
@@ -40,6 +43,19 @@ func (u sprUsecase) getError(payload error) (err error) {
 		return domain.ErrServerError
 	}
 	return payload
+}
+
+func (u sprUsecase) getPlanByID(ctx context.Context, id int64) (res domain.ClassroomSubjectPlan, err error) {
+	res, err = u.plRepo.GetByID(ctx, id)
+	if err != nil {
+		return res, u.getError(err)
+	}
+
+	if res == (domain.ClassroomSubjectPlan{}) {
+		return res, domain.ErrSubjectPlanNotFound
+	}
+
+	return
 }
 
 func (u sprUsecase) Store(c context.Context, spr *domain.ClassroomSubjectPlanResult) (err error) {
@@ -101,13 +117,9 @@ func (u sprUsecase) FetchByPlan(c context.Context, planID int64) (res []domain.C
 	ctx, cancel := context.WithTimeout(c, u.timeout)
 	defer cancel()
 
-	pl, err := u.plRepo.GetByID(ctx, planID)
+	_, err = u.getPlanByID(ctx, planID)
 	if err != nil {
-		return nil, u.getError(err)
-	}
-
-	if pl == (domain.ClassroomSubjectPlan{}) {
-		return nil, domain.ErrSubjectPlanNotFound
+		return nil, err
 	}
 
 	res, err = u.sprRepo.FetchByPlan(ctx, planID)
@@ -116,4 +128,80 @@ func (u sprUsecase) FetchByPlan(c context.Context, planID int64) (res []domain.C
 	}
 	
 	return
+}
+
+func (u sprUsecase) ExportByPlan(c context.Context, planID int64) (token string, err error) {
+	ctx, cancel := context.WithTimeout(c, u.timeout)
+	defer cancel()
+
+	plan, err := u.getPlanByID(ctx, planID)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := u.sprRepo.FetchByPlan(ctx, planID)
+	if err != nil {
+		return "", u.getError(err)
+	}
+
+	students, err := u.sRepo.GetByClassroomAcademic(ctx, plan.Classroom.ID)
+	if err != nil {
+		return "", u.getError(err)
+	}
+
+	g, _ := errgroup.WithContext(c)
+	chanResult := make(chan map[string]interface{})
+	for _, row := range students {
+		row := row
+		plan := plan
+		res := res
+		g.Go(func() error {
+			data := make([]uint, plan.CountPlan)
+			for _, item := range res {
+				if item.Student.ID == row.Student.ID {
+					if item.Index < int(plan.CountPlan) {
+						data[item.Index] = item.Number
+					}
+				}
+			}
+
+			result := map[string]interface{}{
+				"nis": row.Student.SRN,
+				"nama": row.Student.Name,
+				"nilai": data,
+				"rata": 0,
+			}
+			chanResult <- result
+			return nil
+		})
+	}
+
+	go func() {
+		err := g.Wait()
+		if err != nil {
+			return
+		}
+		close(chanResult)
+	}()
+
+	var datas []map[string]interface{}
+	for result := range chanResult {
+		datas = append(datas, result)
+	}
+
+	if err := g.Wait(); err != nil {
+		return "", err
+	}
+
+	path, err := helper.WritePlanResultFileExcel(ctx, plan, datas)
+	if err != nil {
+		return "", u.getError(err)
+	}
+	
+	token, err = helper.CreateFileAccessToken(u.cfg.JWTFileKey, path)
+	if err != nil {
+		return "", u.getError(err)
+	}
+
+	return token, nil
 }
