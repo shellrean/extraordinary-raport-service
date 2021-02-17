92c7e1ded4fc799669b75b574ee52e5eeaf8b30c
shellrean <reanyusufali@gmail.com>
2021-02-17 20:50:02 +0700

    :heavy_plus_sign: add: exschool-student-repository interface

--- domain/exschool_student.go
+++ domain/exschool_student.go
@@ -2,13 +2,19 @@ package domain
 
 import (
 	"time"
+	"context"
 )
 
 type ExschoolStudent struct {
 	ID 			int64
 	Exschool 	Exschool
 	Student 	ClassroomStudent
-	UpdatedBy	User
 	CreatedAt 	time.Time
 	UpdatedAt 	time.Time
+}
+
+type ExschoolStudentRepository interface {
+	FetchByClassroom(ctx context.Context, classroomID int64) ([]ExschoolStudent, error)
+	Store(ctx context.Context, es *ExschoolStudent) (error)
+	Delete(ctx context.Context, id int64) (error)
 }
