257bcb74cd38d06ec858e5f43ca9c42e23cfc2d8
shellrean <reanyusufali@gmail.com>
2021-02-18 11:13:46 +0700

    :heavy_plus_sign: add: method get-by-id store, and delete to exschool-student-repository interface

--- domain/exschool_student.go
+++ domain/exschool_student.go
@@ -15,6 +15,14 @@ type ExschoolStudent struct {
 
 type ExschoolStudentRepository interface {
 	FetchByClassroom(ctx context.Context, classroomID int64) ([]ExschoolStudent, error)
+	GetByID(ctx context.Context, id int64) (ExschoolStudent, error)
+	Store(ctx context.Context, es *ExschoolStudent) (error)
+	Delete(ctx context.Context, id int64) (error)
+}
+
+type ExschoolStudentUsecase interface {
+	FetchByClassroom(ctx context.Context, classroomID int64) ([]ExschoolStudent, error)
+	GetByID(ctx context.Context, id int64) (ExschoolStudent, error)
 	Store(ctx context.Context, es *ExschoolStudent) (error)
 	Delete(ctx context.Context, id int64) (error)
 }
